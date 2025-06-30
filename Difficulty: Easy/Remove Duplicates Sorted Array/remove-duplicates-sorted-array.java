17e2ef5244e9c4117f3104f54b7b72f120b88a70
Swaroop.K.G <swaroop.k.g49@gmail.com>
2025-06-30 11:25:37 +0530

    Update remove-duplicates-sorted-array.java

--- Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.java
+++ Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.java
@@ -7,7 +7,7 @@ class Solution {
         for (int i = 0; i < arr.length; i++) {
             if (!seen.contains(arr[i])) {
                 seen.add(arr[i]);
-                arr[index++] = arr[i]; // move unique elements forward
+                arr[index++] = arr[i];
             }
         }
 
